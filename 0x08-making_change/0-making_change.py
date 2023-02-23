e0a2bf8f6a3689ab4b9fa5c0e1fc777ae7863058
Minte123 <netacademy1986minte@yahoo.com>
2023-02-23 11:39:19 +0300

    Making_Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
 """
-Make changes
+Making Change
 """
 
 
 def makeChange(coins, total):
-    """ make changes """
+    """
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
     if total <= 0:
         return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    def combSum(candidates, target, index, cur, res):
-        """ helper function """
-        if target <= 0:
-            if target == 0:
-                res.append(cur[::])
-            return
-        if res:
-            return
-        for i in range(index, len(candidates)):
-            val = candidates[i]
-            cur.append(val)
-            combSum(candidates, target - val, i, cur, res)
-            last = cur.pop()
-        return res
-    res = combSum(sorted(list(set(coins)), reverse=True), total, 0, [], [])
-    if res:
-        return len(res[0])
-    return -1
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
